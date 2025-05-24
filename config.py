026badf5774c8c9dfa9adf1ab0e4284ca807226e
Mirindra Andrianarivelo <mirindra.andrianarivelo.it@gmail.com>
2025-05-24 05:12:20 +0300

    Delete config.py

--- config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-# Clé secrète pour les sessions
-SECRET_KEY = 'votre_cle_secrete_ici'
-
-# Configuration de la base de données
-basedir = os.path.abspath(os.path.dirname(__file__))
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'instance', 'cantine.db')
